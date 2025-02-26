03df54cdd1a2082199f720c70fa5aee259939e85
Almazyan <almaz@Almazyans-MacBook-Pro.local>
2025-02-26 18:30:38 +0500

    added hw4

---
+++ hw4/import_data/yellow_to_gcs.py
@@ -0,0 +1,126 @@
+import io
+import os
+import requests
+import pandas as pd
+from google.cloud import storage
+import gc
+
+"""
+Pre-reqs: 
+1. `pip install pandas pyarrow google-cloud-storage`
+2. Set GOOGLE_APPLICATION_CREDENTIALS to your project/service-account key
+3. Set GCP_GCS_BUCKET as your bucket or change default value of BUCKET
+"""
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/Users/almaz/Documents/jobs/git/gcp_keys/dbt-user-creds.json"
+
+# services = ['fhv','green','yellow']
+init_url = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/'
+# switch out the bucketname
+BUCKET = os.environ.get("GCP_GCS_BUCKET", "dbt-learn-bucket-zoomcamp")
+
+
+def upload_to_gcs(bucket, object_name, local_file):
+    """
+    Ref: https://cloud.google.com/storage/docs/uploading-objects#storage-upload-object-python
+    """
+    # # WORKAROUND to prevent timeout for files > 6 MB on 800 kbps upload speed.
+    # # (Ref: https://github.com/googleapis/python-storage/issues/74)
+    storage.blob._MAX_MULTIPART_SIZE = 5 * 1024 * 1024  # 5 MB
+    storage.blob._DEFAULT_CHUNKSIZE = 5 * 1024 * 1024  # 5 MB
+
+    client = storage.Client()
+    bucket = client.bucket(bucket)
+    blob = bucket.blob(object_name)
+    blob.upload_from_filename(local_file)
+
+
+def web_to_gcs(year, service):
+    for i in range(12):
+        
+        # sets the month part of the file_name string
+        month = '0'+str(i+1)
+        month = month[-2:]
+
+        # csv file_name
+        file_name = f"{service}_tripdata_{year}-{month}.csv.gz"
+
+        # download it using requests via a pandas df
+        request_url = f"{init_url}{service}/{file_name}"
+        r = requests.get(request_url)
+        open(file_name, 'wb').write(r.content)
+        print(f"Local: {file_name}")
+
+        # Define the schema
+        taxi_dtypes = {
+            'VendorID': pd.Int64Dtype(),
+            'passenger_count': pd.Int64Dtype(),
+            'trip_distance': float,
+            'RatecodeID': pd.Int64Dtype(),
+            'store_and_fwd_flag': str,
+            'PULocationID': pd.Int64Dtype(),
+            'DOLocationID': pd.Int64Dtype(),
+            'payment_type': pd.Int64Dtype(),
+            'fare_amount': float,
+            'extra': float,
+            'mta_tax': float,
+            'tip_amount': float,
+            'tolls_amount': float,
+            'improvement_surcharge': float,
+            'total_amount': float,
+            'congestion_surcharge': float 
+            }
+
+        parse_dates = ['tpep_pickup_datetime', 'tpep_dropoff_datetime']
+
+        # # read it back into a parquet file
+        # df = pd.read_csv(file_name, compression='gzip', dtype=taxi_dtypes, parse_dates=parse_dates, chunksize=100000, low_memory=False)
+        # parquet_file = file_name.replace('.csv.gz', '.parquet')
+        # # df.to_parquet(file_name, engine='pyarrow')
+        # with pd.read_csv(
+        #     file_name, compression='gzip', dtype=taxi_dtypes, 
+        #     parse_dates=parse_dates, chunksize=100000, low_memory=False
+        # ) as reader:
+        #     for i, chunk in enumerate(reader):
+        #         chunk.to_parquet(parquet_file, engine='pyarrow', index=False, append=True if i > 0 else False)
+        # print(f"Parquet: {parquet_file}")
+
+        # Process in chunks to optimize memory
+        parquet_file = file_name.replace('.csv.gz', '.parquet')
+        temp_parquet_files = []
+
+        with pd.read_csv(
+            file_name, compression='gzip', dtype=taxi_dtypes, 
+            parse_dates=parse_dates, chunksize=100000, low_memory=False
+        ) as reader:
+            for chunk_idx, chunk in enumerate(reader):
+                temp_file = f"{parquet_file}_part{chunk_idx}.parquet"
+                chunk.to_parquet(temp_file, engine='pyarrow', index=False)
+                temp_parquet_files.append(temp_file)
+
+        # Combine all chunk files into one
+        combined_df = pd.concat([pd.read_parquet(temp_file) for temp_file in temp_parquet_files])
+        combined_df.to_parquet(parquet_file, engine='pyarrow', index=False)
+        print(f"Parquet: {parquet_file}")
+
+        # Cleanup temporary chunk files
+        for temp_file in temp_parquet_files:
+            os.remove(temp_file)
+
+
+        # upload it to gcs 
+        upload_to_gcs(BUCKET, f"{service}/{parquet_file}", parquet_file)
+        print(f"GCS: {service}/{parquet_file}")
+
+        # Cleanup local files to save disk space
+        os.remove(parquet_file.replace('.parquet', '.csv.gz'))
+        os.remove(parquet_file)
+        #print(f"Cleaned up local files: {file_name})
+
+        # Free memory
+        #gc.collect()
+
+
+# # This script is for Yellow taxi data only... 
+web_to_gcs('2019', 'yellow')
+web_to_gcs('2020', 'yellow')
